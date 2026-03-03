33397507288ad8b95b9b5853ba28cf9f74253aac
insafelgeurgar-jpg <insafelgeurgar@gmail.com>
2026-03-03 14:35:38 +0000

    Add files via upload
    feat:"PROTOTYPE"

---
+++ PROTOTYPE.PHP/connexion.php
@@ -0,0 +1,47 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $dbname = "miniapp";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=".$this->host.";dbname=".$this->dbname.";charset=utf8",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}
+
+class Article {
+    private $conn;
+    private $table = "articles";
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function getAll(){
+        $stmt = $this->conn->query("SELECT * FROM ".$this->table." ORDER BY id DESC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create($title, $content){
+        $sql = "INSERT INTO ".$this->table." (title, content, created_at)
+                VALUES (:title, :content, NOW())";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        return $stmt->execute();
+    }
+}
+?>
